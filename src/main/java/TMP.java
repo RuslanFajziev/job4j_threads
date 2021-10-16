f9d6c27538b6a958819d44572cddbd9f4eb0e2d9
FaizievRT <rus23f@mail.ru>
2021-10-17 00:47:35 +0300

    3. Прерывание нити v5

---
+++ src/main/java/TMP.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class TMP {
+    public static void main(String[] args) throws InterruptedException {
+        Date date = new Date();
+        var currentTime = date.getTime();
+        System.out.println(currentTime);
+        Thread.sleep(2500);
+        Date date2 = new Date();
+        var currentTime2 = date2.getTime();
+        System.out.println(currentTime2);
+        System.out.println(TimeUnit.MILLISECONDS.toDays(currentTime2 - currentTime));
+    }
+}
--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -3,8 +3,7 @@ package ru.job4j.concurrent;
 public class ConsoleProgress implements Runnable {
     @Override
     public void run() {
-        String[] process = new String[]{"\\", "|", "/", "\\", "|", "/", "\\", "|", "/", "\\", "|", "/"};
-
+        String[] process = new String[]{"\\", "|", "/"};
         while (!Thread.currentThread().isInterrupted()) {
             for (int index = 0; index < process.length; index++) {
                 System.out.print("\r load: " + process[index]);
@@ -20,7 +19,7 @@ public class ConsoleProgress implements Runnable {
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        Thread.sleep(1000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        Thread.sleep(50000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
         progress.interrupt();
     }
 }
