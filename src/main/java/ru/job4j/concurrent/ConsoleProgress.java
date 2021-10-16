a9a804acb75a938dbfda880513da105602fb83df
FaizievRT <rus23f@mail.ru>
2021-10-16 21:46:10 +0300

    3. Прерывание нити v4

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -4,6 +4,7 @@ public class ConsoleProgress implements Runnable {
     @Override
     public void run() {
         String[] process = new String[]{"\\", "|", "/", "\\", "|", "/", "\\", "|", "/", "\\", "|", "/"};
+
         while (!Thread.currentThread().isInterrupted()) {
             for (int index = 0; index < process.length; index++) {
                 System.out.print("\r load: " + process[index]);
