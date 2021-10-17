3022a1c4cf036e7e2552b05352fb42bd80c1cd30
FaizievRT <rus23f@mail.ru>
2021-10-17 23:58:09 +0300

    1. Синхронизация общих ресурсов v1

---
+++ src/main/java/ru/job4j/concurrent/Cache.java
@@ -0,0 +1,16 @@
+package ru.job4j.concurrent;
+
+public final class Cache {
+    private static Cache cache;
+
+    public synchronized static Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+
+    public synchronized static Cache getCache() {
+        return cache;
+    }
+}
