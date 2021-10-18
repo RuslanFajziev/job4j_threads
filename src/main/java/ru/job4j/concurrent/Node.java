1d902a795ce542026425e51b204db56e871406de
FaizievRT <rus23f@mail.ru>
2021-10-19 01:04:01 +0300

    3. Immutable объекты v1

---
+++ src/main/java/ru/job4j/concurrent/Node.java
@@ -0,0 +1,19 @@
+package ru.job4j.concurrent;
+
+public class Node<T> {
+    private Node<T> next;
+    private T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
