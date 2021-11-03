65cd13011d32ed9c5a38a7f001267b4b209ee8a3
FaizievRT <rus23f@mail.ru>
2021-11-04 02:12:41 +0300

    3. Junit тест для блокирующей очереди v1

--- src/test/java/ru/job4j/concurrent/queue/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/concurrent/queue/SimpleBlockingQueueTest.java
@@ -2,6 +2,9 @@ package ru.job4j.concurrent.queue;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class SimpleBlockingQueueTest {
@@ -30,5 +33,40 @@ public class SimpleBlockingQueueTest {
         first.join();
         second.join();
         assertEquals((long) simpleBlockingQueue.poll(), 3);
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index < 5; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertEquals(buffer, Arrays.asList(0, 1, 2, 3, 4));
     }
 }
