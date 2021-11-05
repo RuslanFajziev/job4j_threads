7a54090f3ac0e911c8d371896b5270e677395fd5
FaizievRT <rus23f@mail.ru>
2021-11-05 17:15:14 +0300

    1. Неблокирующий кеш v1

--- src/test/java/ru/job4j/concurrent/cas/ConcurrentCacheTest.java
+++ src/test/java/ru/job4j/concurrent/cas/ConcurrentCacheTest.java
@@ -7,36 +7,36 @@ import static org.junit.Assert.*;
 public class ConcurrentCacheTest {
     @Test
     public void test1() {
-        Base model_1 = new Base(1, 1);
-        model_1.setName("Model_1");
-        Base model_2 = new Base(1, 1);
-        model_2.setName("Model_2");
+        Base model1 = new Base(1, 1);
+        model1.setName("Model1");
+        Base model2 = new Base(1, 1);
+        model2.setName("Model2");
         ConcurrentCache cache = new ConcurrentCache();
-        cache.add(model_1);
-        cache.update(model_2);
-        assertEquals(cache.getName(1), "Model_2");
+        cache.add(model1);
+        cache.update(model2);
+        assertEquals(cache.getName(1), "Model2");
     }
 
     @Test
     public void test2() {
-        Base model_1 = new Base(1, 1);
-        model_1.setName("Model_1");
-        Base model_2 = new Base(2, 1);
-        model_2.setName("Model_2");
+        Base model1 = new Base(1, 1);
+        model1.setName("Model1");
+        Base model2 = new Base(2, 1);
+        model2.setName("Model2");
         ConcurrentCache cache = new ConcurrentCache();
-        cache.add(model_1);
-        cache.delete(model_2);
-        assertTrue(cache.add(model_2));
+        cache.add(model1);
+        cache.delete(model2);
+        assertTrue(cache.add(model2));
     }
 
     @Test(expected = OptimisticException.class)
     public void test3() {
-        Base model_1 = new Base(1, 1);
-        model_1.setName("Model_1");
-        Base model_2 = new Base(1, 2);
-        model_2.setName("Model_2");
+        Base model1 = new Base(1, 1);
+        model1.setName("Model1");
+        Base model2 = new Base(1, 2);
+        model2.setName("Model2");
         ConcurrentCache cache = new ConcurrentCache();
-        cache.add(model_1);
-        cache.update(model_2);
+        cache.add(model1);
+        cache.update(model2);
     }
 }
