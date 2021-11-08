a415421041b2d2bcf8f65dd2a3a1c1da5ceb74b3
FaizievRT <rus23f@mail.ru>
2021-11-09 00:50:37 +0300

    2. ExecutorService рассылка почты v1

---
+++ src/main/java/ru/job4j/concurrent/mailpool/EmailNotification.java
@@ -0,0 +1,49 @@
+package ru.job4j.concurrent.mailpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    private ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public void emailTo(User user) {
+        String userName = user.getUsername();
+        String userEmail = user.getEmail();
+        String subject = "Notification " + userName + " to email " + userEmail;
+        String body = "Add a new event to " + userName;
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                send(subject, body, userEmail);
+            }
+        });
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+    public void close(User user) {
+        pool.shutdown();
+    }
+}
+
+class User {
+    private String username;
+    private String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
